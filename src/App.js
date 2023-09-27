52dae1f847fa542ac8f46f860ae67f3431dfcded
Vishnuvardhankm77 <vishnuvardhankm77@gmail.com>
2023-09-27 19:43:30 +0530

    First commit to master branch

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Edupoly</h1>
+      <h2>This is my master branch</h2>
     </div>
   );
 }

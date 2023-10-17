d7ff86b65eaa1848d38d19c724f7a8396aa9ca5a
Vishnuvardhankm77 <vishnuvardhankm77@gmail.com>
2023-10-17 15:14:54 +0530

    todos

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,8 +6,6 @@ function Todo(props){
         <div className="mybox">
             <b>{props.t}</b>
             <button onClick={()=>{props.d(props.i)}}>Delete</button>
-            <button onClick={props}>Undo</button>
-            <button onClick={props}>Done</button>
         </div>
     )
 }

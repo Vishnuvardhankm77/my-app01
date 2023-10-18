c366ffbf54b1e3bf1a2a2533ee2d4b86c61f0d00
Vishnuvardhankm77 <vishnuvardhankm77@gmail.com>
2023-10-18 15:42:05 +0530

    useRefs

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  function abc(){
-    var x=document.getElementById('d1').value;
-    document.getElementById('he1').innerHTML=x;
-  }
-  var [y,setY]=useState('')
-  function abc(v){
-    setY(v)
-  }
+  React.useEffect(()=>{
+    document.getElementById("dd").focus()
+  },[])
   return (
     <div>
       <h1>lets learn form handling in ReactJS</h1>
-      <label htmlFor=''>This is DOM based From Element</label>
-      <input onKeyUp={()=>{abc()}} type="text" id='d1'/>
-      <h1 id='he1'>You Entered:</h1>
-      <hr />
-      <label htmlFor=''>This is React based From Element</label>
-      <input  type="text" onKeyUp={(e)=>{abc(e.target.value)}}/>
-      <h1>You Entered:{y}</h1>
+      <input type="text" id=""dd/>
     </div>
   );
 }
 
-export default App;
+export default App; 

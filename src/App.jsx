f997546c199d27bf1b31956f63ff7fd24ab6357d
Vishnuvardhankm77 <vishnuvardhankm77@gmail.com>
2024-02-19 21:36:37 +0530

    ghjk

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useState } from 'react';
+import React from 'react';
 
 function App() {
-  function abc(){
-    var x=document.getElementById('d1').value;
-    document.getElementById('he1').innerHTML=x;
-  }
-  var [y,setY]=useState('')
-  function abc(v){
-    setY(v)
+  var [ar, setAr] = React.useState(["Cat", "Dog", "Mouse"])
+  function add() {
+   var res=document.getElementById("inp").value
+   setAr([...ar,res])
   }
   return (
     <div>
-      <h1>lets learn form handling in ReactJS</h1>
-      <label htmlFor=''>This is DOM based From Element</label>
-      <input onKeyUp={()=>{abc()}} type="text" id='d1'/>
-      <h1 id='he1'>You Entered:</h1>
-      <hr />
-      <label htmlFor=''>This is React based From Element</label>
-      <input  type="text" onKeyUp={(e)=>{abc(e.target.value)}}/>
-      <h1>You Entered:{y}</h1>
-    </div>
-  );
+      <input type="text" id='inp' />
+      <button onClick={add()}>add</button>
+      {
+        ar.map((a) => {
+          return <li>{a}</li>
+        })
+      }
+      </div>
+  )
 }
 
 export default App;
